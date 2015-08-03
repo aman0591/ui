ad2b23631446a7cb55e2cd6bed8cfd39127d2c68
dularion <antonia.hulha@systaro.de>
2015-08-03 12:22:56 +0200

    trying to fix sh call in converter

--- grails-app/services/streama/VideoConverterService.groovy
+++ grails-app/services/streama/VideoConverterService.groovy
@@ -5,18 +5,25 @@ import grails.transaction.Transactional
 @Transactional
 class VideoConverterService {
 
+  def grailsApplication
+
   def convertToH264(sourceFilePath) {
 
     def process
 
+    String convertVideoH264Path = grailsApplication.mainContext.getResource("tools/convertVideoH264.sh").file.toString()
+
+//    process = 'pwd'.execute()
+//      process = ["$convertVideoH264Path"].execute()
+
     if(Settings.findBySettingsKey('Remove Source After Convert').value == "yes"){
-      process = ['./convertVideoH264.sh', sourceFilePath, true].execute()
+      process = ["$convertVideoH264Path", sourceFilePath, true].execute()
     }else{
-      process = ['./convertVideoH264.sh', sourceFilePath].execute()
+      process = ["$convertVideoH264Path", sourceFilePath].execute()
     }
 
     process.in.eachLine { line ->
-      log.debug(line)
+      log.debug("convertVideoH264: " + line)
     }
 
     log.info('convertToH264 executed with targetFilePath: ' + process.exitValue())
